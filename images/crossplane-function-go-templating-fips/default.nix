a2bb7bdcfc45068189c526aca66d62cb552a994e
Lucas Pick <lucaspick@gmail.com>
2026-07-01 04:28:18 -0500

    fix: build crossplane-function-go-templating at v0.12.2
    Was misconfigured to fetch provider-aws@v1.0.0. Point at
    crossplane-contrib/function-go-templating v0.12.2 (root package) + repair hashes.

--- images/crossplane-function-go-templating-fips/default.nix
+++ images/crossplane-function-go-templating-fips/default.nix
@@ -4,18 +4,18 @@
 # Crossplane provider/component
 
 let
-  version = "1.0.0";
+  version = "0.12.2";
   component = buildGoModule {
     pname = "crossplane-function-go-templating-fips";
     inherit version;
     src = fetchFromGitHub {
       owner = "crossplane-contrib";
-      repo = "provider-aws";
+      repo = "function-go-templating";
       rev = "v${version}";
-      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      hash = "sha256-oda2vtHug0dg/ft249CDBGABudE53sd8Izh7JhaYR8w=";
     };
-    vendorHash = null;
-    subPackages = [ "cmd/provider" ];
+    vendorHash = "sha256-AlNVmh1G9JlFOGy2u9v2INx4dnqKozTyXt46fA3FyYc=";
+    subPackages = [ "." ];
     env.CGO_ENABLED = 1;
     env.GOEXPERIMENT = "boringcrypto";
     ldflags = [ "-s" "-w" ];
